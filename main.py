5adc7ded8e0a51f46ceaecb36ab8d5f629166512
Keith <61766055+wtvlol@users.noreply.github.com>
2020-07-11 17:09:07 +0800

    Added delete txt file after download

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import render_template
 from flask import request
 from flask import send_file
 from flask import jsonify
+import os
 import sys
 import base64
 from youtube_transcript_api import YouTubeTranscriptApi
@@ -154,7 +155,7 @@ def downloadFile(content):
     f.write(content)
     f.close()
     # Send file as response to request.
-    return send_file("./output.txt", as_attachment=True)
+    return send_file("./output.txt", as_attachment=True),,os.remove("./output.txt")
 
 # Basic Base64 decode function
 def decodeBase64(text):
